268f2d49d92a7a9f90555f8a7f6b22d3b0d5b4f8
Plancktonne <47069124+Plancktonne@users.noreply.github.com>
2019-01-27 00:55:21 +0100

    Update Temp1.py

--- Temp1.py
+++ Temp1.py
@@ -18,44 +18,29 @@ def loop():
         while(True):
                 sumCnt += 1 #counting number of reading times
                 chk = dht.readDHT11() #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-                print "**********************************************************************"
+                print ("**********************************************************************")
                 print ("The sumCnt is : %d, \t chk : %d"%(sumCnt,chk))
                 if (chk is dht.DHTLIB_OK): #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
                         print("DHT11,OK!")
                         if (fieldfill < 11):
-                                templist.append(dht.temperature)
-                                humidlist.append(dht.humidity)
-                                fieldfill +=1
+                                if ((dht.temperature < 100) and (dht.humidity < 100):
+                                    templist.append(dht.temperature)
+                                    humidlist.append(dht.humidity)
+                                    fieldfill +=1
                         else:
-                                templist[0] = templist[1]
-                                humidlist[0] = humidlist[1]
-                                templist[1] = templist[2]
-                                humidlist[1] = humidlist[2]
-                                templist[2] = templist[3]
-                                humidlist[2] = humidlist[3]
-                                templist[3] = templist[4]
-                                humidlist[3] = humidlist[4]
-                                templist[4] = templist[5]
-                                humidlist[4] = humidlist[5]
-                                templist[5] = templist[6]
-                                humidlist[5] = humidlist[6]
-                                templist[6] = templist[7]
-                                humidlist[6] = humidlist[7]
-                                templist[7] = templist[8]
-                                humidlist[7] = humidlist[8]
-                                templist[8] = templist[9]
-                                humidlist[8] = humidlist[9]
-                                templist[9] = templist[10]
-                                humidlist[9] = humidlist[10]
-                                templist[10] = dht.temperature
-                                humidlist[10] = dht.humidity
-                                rollingavgtem = 0
-                                rollingavghum = 0
-                                for i in range(0, fieldfill):
-                                        rollingavgtem += templist[i]
-                                        rollingavghum += humidlist[i]
-                                rollingavgtem = round((rollingavgtem / fieldfill), 2)
-                                rollingavghum = round((rollingavghum / fieldfill), 2)
+                                if ((dht.temperature < 100) and (dht.humidity < 100):
+                                    for i in range(0, fieldfill):
+                                        templist[i] = templist[i+1]
+                                        humidlist[i] = humidlist[i+1]                             
+                                    templist[10] = dht.temperature
+                                    humidlist[10] = dht.humidity
+                                    rollingavgtem = 0
+                                    rollingavghum = 0
+                                    for i in range(0, fieldfill):
+                                            rollingavgtem += templist[i]
+                                            rollingavghum += humidlist[i]
+                                    rollingavgtem = round((rollingavgtem / fieldfill), 2)
+                                    rollingavghum = round((rollingavghum / fieldfill), 2)
                 elif(chk is dht.DHTLIB_ERROR_CHECKSUM): #data check has errors
                         print("DHTLIB_ERROR_CHECKSUM!!")
                 elif(chk is dht.DHTLIB_ERROR_TIMEOUT): #reading DHT times out
