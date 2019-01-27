af3239d54a72e33e3b02576ab3d584fdbd53ae49
Plancktonne <47069124+Plancktonne@users.noreply.github.com>
2019-01-27 13:37:35 +0100

    Update Temp1.py

--- Temp1.py
+++ Temp1.py
@@ -81,10 +81,10 @@ PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
 PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
 # Create PCF8574 GPIO adapter.
 try:
-	mcp = PCF8574_GPIO(PCF8574_address)
+	mcp = PCF8574_GPIO(PCF8574A_address)
 except:
 	try:
-		mcp = PCF8574_GPIO(PCF8574A_address)
+		mcp = PCF8574_GPIO(PCF8574_address)
 	except:
 		print ('I2C Address Error !')
 		exit(1)
